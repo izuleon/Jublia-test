15fed581d54708737d63c1dbf6b0b8f10b90cd20
izuleon <izuleonel@gmail.com>
2024-03-25 11:49:06 +0700

    feat: readme (#5)
    fix: broken end point
    fix: increase readability
    
    Co-authored-by: Muhammad Zulfikar <muhammad.zulfikar1593@gmail.com>

--- route/index.py
+++ route/index.py
@@ -1,6 +1,10 @@
-from flask import Blueprint, redirect, url_for
+from datetime import datetime
+
+import pytz
+from flask import Blueprint, jsonify, request
 
 index_bp = Blueprint("index", __name__)
+from route.event_mail import event_mail
 
 
 @index_bp.route("/")
@@ -8,6 +12,24 @@ def index():
     return "hello world"
 
 
-@index_bp.route("/save_emails")
-def redirect_and_save_emails():
-    return redirect(url_for("event_mail.save_emails"))
+@index_bp.post("/save_emails")
+def save_emails():
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"error": "Missing data in request body"}), 400
+
+    # Extract data from request
+    event_id = data.get("event_id")
+    email_subject = data.get("email_subject")
+    email_content = data.get("email_content")
+    timestamp = data.get("timestamp")
+    timestamp_obj = datetime.strptime(timestamp, "%d %b %Y %H:%M")
+    timestamp_obj = timestamp_obj.astimezone(tz=pytz.timezone("Asia/Singapore"))
+    result = event_mail.save_emails(
+        event_id=event_id,
+        email_subject=email_subject,
+        email_content=email_content,
+        sent_at=timestamp_obj,
+    )
+    return result
